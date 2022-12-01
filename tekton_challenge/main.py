8c9c01e83154124195bad60fac9603e8678423b9
Manuel Ochoa <mochoa1127@gmail.com>
2022-12-01 00:13:06 -0500

    test(setup): initial failing tests

--- tekton_challenge/main.py
+++ tekton_challenge/main.py
@@ -1,15 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
 
+from tekton_challenge.config.database import create_tables
+from tekton_challenge.routers import inventory_router
+
+# Database init
+create_tables()
+
+# Init WSGI app
 app = FastAPI()
+
+# Routers
+app.include_router(inventory_router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
---
+++ tests/routers/test_product_inventory.py
@@ -0,0 +1,26 @@
+from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
+
+from tekton_challenge.main import app
+
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_get_product_inventory_by_id():
+    response = client.get("/inventory/products/1")
+    assert response.status_code == HTTP_200_OK
+    assert response.json().get("productId") == 1
+
+
+def test_post_product_inventory():
+    data = {}
+    response = client.post("/inventory/products", json=data)
+    assert response.status_code == HTTP_201_CREATED
+    assert isinstance(response.headers.get("X-Location"), int)
+
+
+def test_put_product_inventory():
+    data = {"productId": 1}
+    response = client.put(f"/inventory/products/{data['productId']}", json=data)
+    assert response.status_code == HTTP_204_NO_CONTENT
